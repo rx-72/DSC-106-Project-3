3bca124a486f8a70561a43979cb7a643f6c4c8cc
Rx-78 <149854255+rx-72@users.noreply.github.com>
2024-02-07 23:05:02 -0800

    Add files via upload

--- App.svelte
+++ App.svelte
@@ -1,8 +1,28 @@
 <script>
     import * as d3 from 'd3';
+    import { onMount } from 'svelte';
+    import Graph from './Graph.svelte';
+
+    let data = [];
+
+    onMount(async () => {
+        const res = await fetch(
+            '/coal_gener_nonnull_filtered.csv',
+        );
+        const csv = await res.text();
+        await d3.csvParse(csv, (d) => {
+            data.push({
+               year: new Date(Date.UTC(d["year"])),
+               country: d["country"],
+               electricity_generated: d["coal_electricity"], 
+            });
+        });
+        data = data;
+    });
+
+    $: console.log(data)
 </script>
 
 <main>
-  <h1>DSC106 Project 3</h1>
-  <Graph {data} />
+    <Graph {data} />
 </main>
